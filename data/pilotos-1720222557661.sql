5b6685c81b5f4fa4438af4cc32625c9d73d041eb
Aylen Fiscina <a.fiscina@fi.uba.ar>
2024-07-05 20:42:17 -0300

    nuevos pilotos

---
+++ data/pilotos-1720222557661.sql
@@ -0,0 +1,22 @@
+INSERT INTO public.pilotos ("firstName","lastName",city,team,podiums,world_champions) VALUES
+	 ('Max','Verstappen','Paises Bajos','Red Bull Racing',106,3),
+	 ('Oscar','Piastri','Australia','McLaren',4,0),
+	 ('Lando','Norris','Reino Unido','McLaren',19,0),
+	 ('Sergio','Perez','Mexico','Red Bull Racing',39,0),
+	 ('Charles','Leclerc','Monaco','Ferrari',35,0),
+	 ('Carlos','Sainz','España','Ferrari',23,0),
+	 ('George','Russell','Reino Unido','Mercedes',13,0),
+	 ('Lewis','Hamilton','Reino Unido','Mercedes',198,7),
+	 ('Fernando','Alonso','España','Aston Martin',106,2),
+	 ('Lance','Stroll','Canada','Aston Martin',3,0);
+INSERT INTO public.pilotos ("firstName","lastName",city,team,podiums,world_champions) VALUES
+	 ('Yuki','Tsunoda','Japan','RB',0,0),
+	 ('Daniel','Ricciardo','Australia','RB',32,0),
+	 ('Nico','Hulkenberg','Alemania','Haas',0,0),
+	 ('Kevin','Magnussen','Dinamarca','Haas',1,0),
+	 ('Pierre','Gasly','Francia','Alpine',4,0),
+	 ('Esteban','Ocon','Francia','Alpine',3,0),
+	 ('Alexander','Albon','Tailandia','Williams',2,0),
+	 ('Logan','Sargeant','Estados Unidos','Williams',0,0),
+	 ('Zhou','Guanyu','China','Kick Sauber',0,0),
+	 ('Valtteri','Bottas','Finlandia','Kick Sauber',67,0);
