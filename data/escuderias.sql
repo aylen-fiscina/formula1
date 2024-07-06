d329f482ff3eeb38ba6ce3d1b9158839eb7b067b
Aylen Fiscina <a.fiscina@fi.uba.ar>
2024-07-05 21:52:37 -0300

    Add data escuderias

---
+++ data/escuderias.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.escuderias (full_team_name,world_championships_team,team_chief) VALUES
+	 ('Red Bull Racing',6,'Christian Horner'),
+	 ('Ferrari',16,'Frédéric Vasseur'),
+	 ('McLaren',8,'Andrea Stella'),
+	 ('Mercedes',8,'Toto Wolff'),
+	 ('Aston Martin',0,'Mike Krack'),
+	 ('RB',0,'Laurent Mekies'),
+	 ('Haas',0,'Ayao Komatsu'),
+	 ('Alpine',2,'Bruno Famin'),
+	 ('Williams',9,'James Vowles'),
+	 ('Kick Sauber',0,'Alessandro Alunni Bravi');
