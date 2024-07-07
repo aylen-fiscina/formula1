5e7bb4fdba1da0b38976834332e9d5e0a85b075c
Aylen Fiscina <a.fiscina@fi.uba.ar>
2024-07-07 19:24:05 -0300

    Add images escuderias y pilotos

--- data/escuderias.sql
+++ data/escuderias.sql
@@ -1,11 +1,11 @@
-INSERT INTO public.escuderias (full_team_name,world_championships_team,team_chief) VALUES
-	 ('Red Bull Racing',6,'Christian Horner'),
-	 ('Ferrari',16,'Frédéric Vasseur'),
-	 ('McLaren',8,'Andrea Stella'),
-	 ('Mercedes',8,'Toto Wolff'),
-	 ('Aston Martin',0,'Mike Krack'),
-	 ('RB',0,'Laurent Mekies'),
-	 ('Haas',0,'Ayao Komatsu'),
-	 ('Alpine',2,'Bruno Famin'),
-	 ('Williams',9,'James Vowles'),
-	 ('Kick Sauber',0,'Alessandro Alunni Bravi');
+INSERT INTO public.escuderias (full_team_name,world_championships_team,team_chief,image_url_escuderia) VALUES
+	 ('Red Bull Racing',6,'Christian Horner','https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/red%20bull.png'),
+	 ('Ferrari',16,'Frédéric Vasseur','https://media.formula1.com/image/upload/f_auto,c_limit,q_auto,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/ferrari.png'),
+	 ('McLaren',8,'Andrea Stella','https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/mclaren.png'),
+	 ('Mercedes',8,'Toto Wolff','https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/mercedes.png'),
+	 ('Aston Martin',0,'Mike Krack','https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/aston%20martin%202024.png'),
+	 ('RB',0,'Laurent Mekies','https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/rb.png'),
+	 ('Haas',0,'Ayao Komatsu','https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/haas.png'),
+	 ('Alpine',2,'Bruno Famin','https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/alpine.png'),
+	 ('Williams',9,'James Vowles','https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/williams.png'),
+	 ('Kick Sauber',0,'Alessandro Alunni Bravi','https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_1320/content/dam/fom-website/2018-redesign-assets/team%20logos/kick%20sauber.png');
