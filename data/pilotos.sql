212b0d0364bd5290639d8d79b11e29729d852234
Aylen Fiscina <a.fiscina@fi.uba.ar>
2024-07-11 19:20:20 -0300

    cambio en la base de datos de pilotos

--- data/pilotos.sql
+++ data/pilotos.sql
@@ -1,16 +1,5 @@
 INSERT INTO public.piloto (nombre,apellido,ciudad,id_escuderia,podios,campeonatos_mundiales, numero, imagen) VALUES
-	 ('Max','Verstappen','Paises Bajos',1,106,3,1,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png'),
-	 ('Oscar','Piastri','Australia',3,4,0,81,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/O/OSCPIA01_Oscar_Piastri/oscpia01.png'),
-	 ('Lando','Norris','Reino Unido',3,19,0,4,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANNOR01_Lando_Norris/lannor01.png'),
-	 ('Sergio','Perez','Mexico',1,39,0,11,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/S/SERPER01_Sergio_Perez/serper01.png'),
-	 ('Charles','Leclerc','Monaco',2,35,0,16,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png'),
-	 ('Carlos','Sainz','España',2,23,0,55,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CARSAI01_Carlos_Sainz/carsai01.png'),
-	 ('George','Russell','Reino Unido',4,13,0,63,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/G/GEORUS01_George_Russell/georus01.png'),
-	 ('Lewis','Hamilton','Reino Unido',4,198,7,44,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LEWHAM01_Lewis_Hamilton/lewham01.png'),
-	 ('Fernando','Alonso','España',5,106,2,14,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/F/FERALO01_Fernando_Alonso/feralo01.png'),
-	 ('Lance','Stroll','Canada',5,3,0,18,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANSTR01_Lance_Stroll/lanstr01.png');
-INSERT INTO public.piloto (nombre,apellido,ciudad,id_escuderia,podios,campeonatos_mundiales, numero, imagen) VALUES
-	 ('Yuki','Tsunoda','RB',6,0,0,22,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/Y/YUKTSU01_Yuki_Tsunoda/yuktsu01.png'), 
+	 ('Yuki','Tsunoda','Japon',6,0,0,22,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/Y/YUKTSU01_Yuki_Tsunoda/yuktsu01.png'), 
 	 ('Daniel','Ricciardo','Australia',6,32,0,3,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/D/DANRIC01_Daniel_Ricciardo/danric01.png'), 
 	 ('Nico','Hulkenberg','Alemania',7,0,0,27,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/N/NICHUL01_Nico_Hulkenberg/nichul01.png'), 
 	 ('Kevin','Magnussen','Dinamarca',7,1,0,20,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/K/KEVMAG01_Kevin_Magnussen/kevmag01.png'), 
@@ -20,3 +9,14 @@ INSERT INTO public.piloto (nombre,apellido,ciudad,id_escuderia,podios,campeonato
 	 ('Logan','Sargeant','Estados Unidos',9,0,0,2,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LOGSAR01_Logan_Sargeant/logsar01.png'), 
 	 ('Zhou','Guanyu','China',10,0,0,24,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/G/GUAZHO01_Guanyu_Zhou/guazho01.png'), 
 	 ('Valtteri','Bottas','Finlandia',10,67,0,77,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/V/VALBOT01_Valtteri_Bottas/valbot01.png');
+	 INSERT INTO public.piloto (nombre,apellido,ciudad,id_escuderia,podios,campeonatos_mundiales, numero, imagen) VALUES
+	 ('Max','Verstappen','Paises Bajos',1,106,3,1,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png'),
+	 ('Sergio','Perez','Mexico',1,39,0,11,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/S/SERPER01_Sergio_Perez/serper01.png'),
+	  ('Charles','Leclerc','Monaco',2,35,0,16,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png'),
+	 ('Carlos','Sainz','España',2,23,0,55,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CARSAI01_Carlos_Sainz/carsai01.png'),
+	 ('Oscar','Piastri','Australia',3,4,0,81,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/O/OSCPIA01_Oscar_Piastri/oscpia01.png'),
+	 ('Lando','Norris','Reino Unido',3,19,0,4,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANNOR01_Lando_Norris/lannor01.png'),
+	 ('George','Russell','Reino Unido',4,13,0,63,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/G/GEORUS01_George_Russell/georus01.png'),
+	 ('Lewis','Hamilton','Reino Unido',4,198,7,44,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LEWHAM01_Lewis_Hamilton/lewham01.png'),
+	 ('Fernando','Alonso','España',5,106,2,14,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/F/FERALO01_Fernando_Alonso/feralo01.png'),
+	 ('Lance','Stroll','Canada',5,3,0,18,'https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANSTR01_Lance_Stroll/lanstr01.png');
